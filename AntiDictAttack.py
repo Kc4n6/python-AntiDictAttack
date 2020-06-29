116d45170531d5eeb4dadc1bc387425c677ea501
Kc4n6 <56691237+Kc4n6@users.noreply.github.com>
2020-06-29 16:05:31 +0300

    Update AntiDictAttack.py

--- AntiDictAttack.py
+++ AntiDictAttack.py
@@ -23,18 +23,18 @@ def Mail_At():
     Mail_icerigi="son birkaC dakika icerisinde fazla sayida giris denemesi yapildi..."
     icerik = MIMEText(Mail_icerigi,"plain")
     mail.attach(icerik)
-    print("buraya kadar sikinti yok kenks")
+    
     try:
         gonder = smtplib.SMTP("smtp.gmail.com",587)
-        print("bunu da yaptik kenks")
+        
         gonder.ehlo()
-        print("ehlo da tamam kenks")
+       
         gonder.starttls()
-        print("tlssi de baslattik kenks")
+      
         gonder.login("Buraya mail adresini yaz","buraya parolani yaz")
-        print("login olduk kenks")
+      
         gonder.sendmail(mail["From"],mail["To"],mail.as_string())
-        print("maili de attik kenksss.")
+      
         gonder.close()
     except:
         print("mail gonderme isleminde bir problem var lutfen kontrol saglayiniz.")    
